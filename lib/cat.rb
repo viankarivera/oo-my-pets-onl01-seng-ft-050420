cd45cf15a68dc32657e795cdaa80e27653cc452c
Vianka Rivera <viankalrivera@gmail.com>
2020-05-21 16:31:33 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner)
+    @owner = owner
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
+
+
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner)
+    @owner = owner
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
+
 end
