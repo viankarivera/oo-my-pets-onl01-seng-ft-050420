11a925561a7a506785d17252c871ccce6d624864
Vianka Rivera <viankalrivera@gmail.com>
2020-05-21 16:45:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,7 +45,7 @@ class Owner
   end
 
   def sell_pets
-    @pets.each do |type, name_array|
+    @pets.each do |type, name|
       name_array.each do |pet|
         pet.mood = "nervous"
         #name_array.delete(pet)
